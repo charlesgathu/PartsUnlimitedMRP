907382a186020c52cef3fd6305e8c7b3a94560ba
charlesgathu <7286291+charlesgathu@users.noreply.github.com>
2018-06-23 10:48:55 +0300

    Update install_mrp_dependencies.sh

--- docs/HOL_Continuous-Deployment/env/install_mrp_dependencies.sh
+++ docs/HOL_Continuous-Deployment/env/install_mrp_dependencies.sh
@@ -10,5 +10,6 @@ apt-get install tomcat7 -y
 apt-get install wget -y
 
 # Set Java environment variables
+export DEBIAN_FRONTEND=noninteractive
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
